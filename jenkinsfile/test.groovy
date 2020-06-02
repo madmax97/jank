126f5c1b1465e1e474d1333888563c4fcce54f11
Бурак Илья Александрович <burak.i.al@sberbank.ru>
2020-06-02 18:22:58 +0500

    code section

--- jenkinsfile/test.groovy
+++ jenkinsfile/test.groovy
@@ -1,18 +1,22 @@
 pipeline {
     agent{node('master')}
     stages {
-    stage ('Download from git'){
-        checkout([$class: 'GitSCM',
-                    branches: [[name: '*/master']],
-                    doGenerateSubmoduleConfigurations: false,
-                    extensions: [[$class: 'RelativeTargetDirectory',
-                    relativeTargetDir: 'test']],
-                    submoduleCfg: [],
-                    userRemoteConfigs: [[credentialsId: 'IlyaBurakGit',url: 'https://github.com/ilyaburak/study_jenkins.git']]])
-                
-      }
-        
-       
+        steps {
+            script {
+                cleanWs()
+                //git(branch: 'master', credentialsId: 'IlyaBurakGit', url: 'https://github.com/ilyaburak/study_jenkins.git')
+            }
+            script {
+                echo 'Start download project'
+                checkout([$class                           : 'GitSCM',
+                          branches                         : [[name: '*/master']],
+                          doGenerateSubmoduleConfigurations: false,
+                          extensions                       : [[$class           : 'RelativeTargetDirectory',
+                                                               relativeTargetDir: 'auto']],
+                          submoduleCfg                     : [],
+                          userRemoteConfigs                : [[credentialsId: 'IlyaBurakGit', url: 'https://github.com/ilyaburak/study_jenkins.git']]])
+            }
+        }
     }
     
 }
