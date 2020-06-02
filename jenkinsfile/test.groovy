0e3b49a60e26289d2a4c9b277d4ffb53e12c14fd
Бурак Илья Александрович <burak.i.al@sberbank.ru>
2020-06-02 18:25:20 +0500

    code section

--- jenkinsfile/test.groovy
+++ jenkinsfile/test.groovy
@@ -1,20 +1,22 @@
 pipeline {
     agent{node('master')}
     stages {
-        steps {
-            script {
-                cleanWs()
-                //git(branch: 'master', credentialsId: 'IlyaBurakGit', url: 'https://github.com/ilyaburak/study_jenkins.git')
-            }
-            script {
-                echo 'Start download project'
-                checkout([$class                           : 'GitSCM',
-                          branches                         : [[name: '*/master']],
-                          doGenerateSubmoduleConfigurations: false,
-                          extensions                       : [[$class           : 'RelativeTargetDirectory',
-                                                               relativeTargetDir: 'auto']],
-                          submoduleCfg                     : [],
-                          userRemoteConfigs                : [[credentialsId: 'IlyaBurakGit', url: 'https://github.com/ilyaburak/study_jenkins.git']]])
+        stage {
+            steps {
+                script {
+                    cleanWs()
+                    //git(branch: 'master', credentialsId: 'IlyaBurakGit', url: 'https://github.com/ilyaburak/study_jenkins.git')
+                }
+                script {
+                    echo 'Start download project'
+                    checkout([$class                           : 'GitSCM',
+                              branches                         : [[name: '*/master']],
+                              doGenerateSubmoduleConfigurations: false,
+                              extensions                       : [[$class           : 'RelativeTargetDirectory',
+                                                                   relativeTargetDir: 'auto']],
+                              submoduleCfg                     : [],
+                              userRemoteConfigs                : [[credentialsId: 'IlyaBurakGit', url: 'https://github.com/ilyaburak/study_jenkins.git']]])
+                }
             }
         }
     }
