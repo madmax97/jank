c9921a22c5812d4c582cb568632ffedf25381299
Бурак Илья Александрович <burak.i.al@sberbank.ru>
2020-06-02 18:26:30 +0500

    code section

--- jenkinsfile/test.groovy
+++ jenkinsfile/test.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent{node('master')}
     stages {
-        stage {
+        stage('Dowload project') {
             steps {
                 script {
                     cleanWs()
