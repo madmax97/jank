c85a2df343ea24f4030357deeaa243a334b31fd8
madmax97 <maks.shushenachev@yandex.ru>
2020-06-30 18:06:58 +0500

    Update test.groovy

--- jenkinsfile/test.groovy
+++ jenkinsfile/test.groovy
@@ -1,15 +1,26 @@
-    
 pipeline {
     agent{node('master')}
-    stages {
-        stage('Dowload project') {
-            steps {
+    stages { 
+        stage('Download git repository') {
+            steps { 
                 script {
                     cleanWs()
-                    //git(branch: 'master', credentialsId: 'IlyaBurakGit', url: 'https://github.com/ilyaburak/study_jenkins.git')
+                    withCredentials([
+                        usernamePassword(credentialsId: 'srv_sudo',
+                        usernameVariable: 'username',
+                        passwordVariable: 'password')
+                    ]) {
+                        try {
+                            sh "echo '${password}' | sudo -S docker stop max_d"
+                            sh "echo '${password}' | sudo -S docker container rm max_d"
+                        } catch (Exception e) {
+                            print 'problem'
+                            currentBuild.result = 'FAILURE'
+                        }
+
+                    }
                 }
                 script {
-                    echo 'Start download project'
                     checkout([$class                           : 'GitSCM',
                               branches                         : [[name: '*/master']],
                               doGenerateSubmoduleConfigurations: false,
@@ -19,18 +30,52 @@ pipeline {
                               userRemoteConfigs                : [[credentialsId: 'ShushenachevMaxGit', url: 'https://github.com/madmax97/jank.git']]])
                 }
             }
-        }
-        stage ('Create docker image'){
+        }     
+        
+         stage ('Create docker image'){
             steps{
                 script{
-                    sh "docker build ${WORKSPACE}/auto -t webapp"
-                    sh "docker run -d webapp"
-                    // sh "docker exec -it webapp "df -h > ~/proc""
+                    withCredentials([
+                        usernamePassword(credentialsId: 'srv_sudo',
+                        usernameVariable: 'username',
+                        passwordVariable: 'password')
+                    ]) {
+
+                        sh "echo '${password}' | sudo -S docker build ${WORKSPACE}/auto -t docker_new"
+                        sh "echo '${password}' | sudo -S docker run -d -p 8008:80 --name max_d -v /home/adminci/v_dir:/stat docker_new"
+                    }
+                }
+            }
+        }      
+    
+    stage ('Write info to file'){
+            steps{
+                script{
+                    withCredentials([
+                        usernamePassword(credentialsId: 'srv_sudo',
+                        usernameVariable: 'username',
+                        passwordVariable: 'password')
+                    ]) {
+                        sh "echo '${password}' | sudo -S docker exec -t max_d bash -c 'df -h > /stat/v_file.txt'"
+                        sh "echo '${password}' | sudo -S docker exec -t max_d bash -c '\n top -n 1 -b >> /stat/v_file.txt'"                       
+                    }
+                }
+            }
+        }
+        
+        stage ('Stop'){
+            steps{
+                script{
+                    withCredentials([
+                        usernamePassword(credentialsId: 'srv_sudo',
+                        usernameVariable: 'username',
+                        passwordVariable: 'password')
+                    ]) {
+                        sh "echo '${password}' | sudo -S docker stop max_d"                 
+                    }
                 }
             }
         }
         
     }
-
-    
 }
